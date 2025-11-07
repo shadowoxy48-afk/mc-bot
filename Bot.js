9e28d79548ae24a26a4fcbf41b8a06da991272d4
shadowoxy48-afk <shadowoxy48@gmail.com>
2025-11-08 01:11:07 +0300

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -1,10 +1,10 @@
-import mineflayer from "mineflayer";
+const mineflayer = require('mineflayer');
 
 function startBot() {
   const bot = mineflayer.createBot({
     host: "xh1n.aternos.me", // Sunucu IP'si
-    port: 59020, // Bedrock değilse 25565 yap
-    username: "ShadowAfk", // Botun kullanıcı adı
+    port: 59020,             // Port
+    username: "ShadowAfk",   // Botun ismi
   });
 
   bot.on("login", () => console.log("[BOT] Sunucuya giriş yaptı!"));
